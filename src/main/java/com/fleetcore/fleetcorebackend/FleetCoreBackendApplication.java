fd407e3c63e6f369f4377df8a77fcfad6f0086d7
andrei <andreimachidon@gmail.com>
2023-09-06 00:57:18 +0300

    initial-commit

---
+++ src/main/java/com/fleetcore/fleetcorebackend/FleetCoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fleetcore.fleetcorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetCoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetCoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fleetcore/fleetcorebackend/FleetCoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleetcore.fleetcorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetCoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
